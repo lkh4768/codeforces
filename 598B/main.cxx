7a9dd2dc4abd854f6d6b0f3a9d9e9b166214957d
lkh4768 <lkh5510@gmail.com>
2017-06-13 11:03:48 +0900

    update 598B

--- 598B/main.cxx
+++ 598B/main.cxx
@@ -5,22 +5,14 @@ int main()
 {
 	char s[10001] = {0};
 	char rs[10001] = {0};
-	int si[10000];
-	int m = 0, l = 0, r = 0, k = 0, size = 0;
+	int m = 0, l = 0, r = 0, k = 0;
 	std::cin >> s >> m;
-
-	size = (int)strlen(s);
 
 	for(int i = 0; i < m; i++)
 	{
-		for(int j = 0; j < size; j++)
-			si[j] = j;
-
 		std::cin >> l >> r >> k;
-		for(int j = l-1; j <= r-1; j++) { si[j] = (l-1) + ((si[j]-l+1+k) % (r-l+1)); }
-
-		for(int j = 0; j < size; j++) rs[si[j]] = s[j];
-		strcpy(s, rs);
+		for(int j = l-1; j <= r-1; j++) rs[(l-1) + ((j-l+1+k) % (r-l+1))] = s[j]; 
+		for(int j = l-1; j <= r-1; j++) s[j] = rs[j];
 	}
 
 	std::cout << s << std::endl;
