260469939d89edbae42283f2fa02a24d041df89d
lkh4768 <lkh5510@gmail.com>
2017-09-13 02:18:14 +0000

    resolved 4d

--- 4/D/main.go
+++ 4/D/main.go
@@ -2,23 +2,86 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
 var in = bufio.NewScanner(os.Stdin)
 
+var n, w, h, max, maxi, elen int
+var e [5001][3]int
+var dep [5001]int
+var prev [5001]int
+var ret [5001]int
+
+type Matrix [5001][3]int
+
 func init() {
 	in.Split(bufio.ScanWords)
+	for i := range dep {
+		dep[i] = 1
+	}
+
+	for i := range prev {
+		prev[i] = -1
+	}
 }
 
+func (m Matrix) Len() int           { return elen }
+func (m Matrix) Less(i, j int) bool { return m[i][0] < m[j][0] }
+func (m *Matrix) Swap(i, j int)     { m[i], m[j] = m[j], m[i] }
+
 func main() {
-	maxSum, maxIdx := -1, -1
-	n, w, h := readInt(), readInt(), readInt()
-	var e [5001][3]int
+	n, w, h = readInt(), readInt(), readInt()
 
 	for i := 0; i < n; i++ {
-		e[i][0], e[i][1], e[i][2] = readInt(), readInt(), i
+		tw, th := readInt(), readInt()
+		if tw > w && th > h {
+			e[i][0], e[i][1], e[i][2] = tw, th, i
+			elen++
+		}
+	}
+
+	if elen == 0 {
+		fmt.Println("0")
+		return
+	}
+
+	m := Matrix(e)
+	sort.Sort(&m)
+
+	for i := 1; i < elen; i++ {
+		for j := 0; j < i; j++ {
+			if m[i][0] > m[j][0] && m[i][1] > m[j][1] && dep[i] < dep[j]+1 {
+				dep[i] = dep[j] + 1
+				prev[i] = j
+			}
+		}
+	}
+
+	for i := 0; i < elen; i++ {
+		if max < dep[i] {
+			max = dep[i]
+			maxi = i
+		}
+	}
+
+	i := maxi
+	j := 0
+	for {
+		ret[j] = m[i][2]
+		i = prev[i]
+		if i == -1 {
+			break
+		}
+		j++
+	}
+
+	fmt.Printf("%d\n", max)
+	for i := j; i >= 0; i-- {
+		fmt.Printf("%d ", ret[i])
 	}
 }
 
